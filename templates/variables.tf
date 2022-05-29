02102f006675d6492824f047c6d8e63408769646
Nhat TONG <nhat.tong@sword-group.com>
2022-05-29 16:08:48 +0200

    change region

--- templates/variables.tf
+++ templates/variables.tf
@@ -4,7 +4,7 @@
 
 variable "onpremise_location" {
     type = string
-    default = "westcentralus"
+    default = "northeurope"
     description = "On-premise resources location"
 }
 
@@ -16,7 +16,7 @@ variable "onpremise_bgp_asn" {
 
 variable "azure_location" {
     type = string
-    default = "eastus2"
+    default = "westeurope"
     description = "Azure resources location"
 }
 
